72eec392cea8ab75859d39ceed025c4f2d4a1bbb
Lauren N. Roth <laurennicoleroth@gmail.com>
2016-05-18 17:23:29 -0400

    Add Google Places API constants

--- ScavengerHunt/WelcomeViewController.swift
+++ ScavengerHunt/WelcomeViewController.swift
@@ -13,7 +13,7 @@ class WelcomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        print(Constants.Keys.GoogleKey)
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,15 +21,5 @@ class WelcomeViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
